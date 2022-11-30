42f2429724411f824336abe2d3e7fcdc909432ee
bilel <bilel.moudache0412@gmail.com>
2022-11-30 21:47:54 +0100

    fix duplicat card

--- book.js
+++ book.js
@@ -1,7 +1,4 @@
-
-const theHobbit = new book("The Hobbit","J.R.R. Tolkien","296","not read yet");
-const theHobbit2 = new book("The Hobbit","J.R.R. Tolkien","295","not read yet");
-let myLibrary = [theHobbit,theHobbit2];
+let myLibrary = [];
 
 function book(title,author,pages,read){
     this.title = title;
@@ -15,29 +12,38 @@ function book(title,author,pages,read){
 }
 
 function addBookToLibrary(book) {
-  myLibrary.push(book);
+    myLibrary.push(book);
+    clearCard();
+    displayMyLibrary();
 }
-
+function clearCard(){
+    const bodyClass =document.querySelector('.body');
+    bodyClass.innerHTML = '';
+}
 function displayMyLibrary(){
-    const bodyClass =document.querySelector('.body')
+    const bodyClass =document.querySelector('.body');
     for(i = 0 ;i < myLibrary.length ;i++){
         const div = document.createElement('div');
         div.classList.add('card');
-        div.innerHTML = `<ul><li>Titre : ${myLibrary[i].title}</li><li>Auteur : ${myLibrary[i].author}</li><li>Pages :${myLibrary[i].pages}</li><li>Lu :${myLibrary[i].read}</li></ul>`
-        bodyClass.appendChild(div)
+        div.innerHTML = `<ul><li>Titre<br><em>${myLibrary[i].title}</em></li><li>Auteur<br><em>${myLibrary[i].author}</em></li><li>Pages<br><em>${myLibrary[i].pages}</em></li><li>Lu<br><em>${myLibrary[i].read}</em></li></ul>`
+        bodyClass.appendChild(div);
     }
 }
-
-
 
 const body = document.querySelector('body');
 
 function openForm(){
     const div = document.createElement('div');
     div.classList.add('form');
-    div.innerHTML = '<form action=""><fieldset><legend>BOOK</legend><div class="close">❌</div><label for="titre">Titre</label><input type="text" id="titre" name="title"><label for="auteur">Auteur</label><input type="text" id="auteur" name="author"><label for="pages">Pages</label><input type="number" id="pages" name="pages"><label for="lu">Lu</label><input type="text" id="lu" name="read"><button>Valider</button></fieldset></form>';
+    div.innerHTML = '<form action=""><fieldset><legend>BOOK</legend><div class="close">❌</div><label for="titre">Titre</label><input type="text" id="titre" name="title"><label for="auteur">Auteur</label><input type="text" id="auteur" name="author"><label for="pages">Pages</label><input type="number" id="pages" name="pages"><label for="lu">Lu</label><input type="text" id="lu" name="read"><button type="submit">Valider</button></fieldset></form>';
     body.appendChild(div);
     const cross = document.querySelector('.close')
+    const submitFormBtn = document.querySelector('.form > form > fieldset > button')
+    submitFormBtn.addEventListener('click',function(e){
+        e.preventDefault();
+        getValue();
+        closeForm();
+    })
     cross.addEventListener('click',function(){
         closeForm();
     })  
@@ -48,12 +54,20 @@ function closeForm(){
     body.removeChild(div);
 }
 
-const btnOpenForm = document.querySelector('.body > .btn');
+function getValue(){
+    let title = document.getElementById('titre').value;
+    let author = document.getElementById('auteur').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('lu').value;
+    let newBook = new book(title,author,pages,read);
+    addBookToLibrary(newBook);
+}
+
+const btnOpenForm = document.querySelector('.btn');
 
 btnOpenForm.addEventListener('click',function(){
     openForm();
 });
-
 
 
 
